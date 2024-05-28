d69192ac3f55263733d2cd2460f15d7f7d7652ba
pedro <pedrolucas.xs@gmail.com>
2024-05-28 16:25:22 -0300

    fix/media queries added

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,18 @@ import Innercard from "./components/Innercard/Innercard"
 function App(props) {
 
   return (
-    <div className={`w-3/6 h-4/6 rounded-lg border-x-slate-100 
-                    shadow-[0px_0px_18px_2px_rgba(0,0,0,0.53)] grid grid-cols-3
-                    hover:scale-105 duration-700`}>
-                      <Innercard titulo="Básico" preco="19,90" descricao="Nosso plano básico"
-                      item1="Lorem ipsum" item2="Nullam quis" item3="Quisque ac leo" 
-                      item4="Mauris at nisl"/>
-                      <Innercard titulo="Intermediário" preco="39,90" descricao="+Economia +Vantagens"
-                      item1="Mauris at nisl" item2="Nullam quis" item3="Quisque ac leo" item4="+ Básico"/>
-                      <Innercard titulo="Profissional" preco="59,90" descricao="Para você que sabe escolher"
-                      item1="Nullam quis" item2="Lorem ipsum" item3="+ Básico" item4="+ Intermediário"/>
+    <div className="w-5/6 h-screen flex flex-col rounded-lg border-x-slate-100 
+    hover:translate-y-7 duration-700 lg:w-4/6 lg:h-4/6 lg:flex-row lg:items-center 
+    lg:justify-center lg:shadow-[0px_0px_18px_2px_rgba(0,0,0,0.53)] lg:hover:hover:translate-y-0">
+
+      <Innercard titulo="Básico" preco="19,90" descricao="Nosso plano básico"
+        item1="Lorem ipsum" item2="Nullam quis" item3="Quisque ac leo"
+        item4="Mauris at nisl" />
+      <Innercard titulo="Intermediário" preco="39,90" descricao="+Economia +Vantagens"
+        item1="Mauris at nisl" item2="Nullam quis" item3="Quisque ac leo" item4="+ Básico" />
+      <Innercard titulo="Profissional" preco="59,90" descricao="Para você que sabe escolher"
+        item1="Nullam quis" item2="Lorem ipsum" item3="+ Básico" item4="+ Intermediário" />
+
     </div>
   )
 }
--- src/components/Innercard/Innercard.jsx
+++ src/components/Innercard/Innercard.jsx
@@ -2,14 +2,19 @@ import { IoMdCheckmarkCircleOutline } from "react-icons/io";
 
 export default (props) => {
     return(
-        <div className="flex flex-col items-center space-y-10 p-4 hover:bg-indigo-700 
-        hover:text-white duration-300 hover:-translate-y-2 hover:rounded hover:scale-105">
-            <div className="flex flex-col gap-1 bg-indigo-600 text-white rounded p-1 w-4/5 text-center">
+        <div className="flex flex-col items-center space-y-7 p-2 hover:bg-indigo-700 
+        hover:text-white duration-300 hover:-translate-y-2 hover:rounded hover:scale-105
+        hover:opacity-90 lg:w-1/3 lg:h-full lg:hover:opacity-100 lg:hover:-translate-y-0
+        lg:space-y-0 lg:justify-between">
+
+            <div className="flex flex-col bg-indigo-600 text-white rounded p-5 w-5/6 text-center
+            lg:h-1/6">
                 <h3 className="uppercase text-white font-black text-1xl">{props.titulo}</h3>
                 <h1 className="uppercase font-medium text-1xl">R$ {props.preco} / mês</h1>
             </div>
 
-            <div className="p-2 rounded flex flex-col gap-3 border-t-2 border-b-2 border-solid border-indigo-300">
+            <div className="p-2 rounded flex flex-col gap-3 border-t-2 border-b-2 border-solid 
+            border-indigo-300 lg:h-4/6 lg:justify-center">
                 <h4>{props.descricao}</h4>
                 <div className="flex items-center gap-2">
                     <IoMdCheckmarkCircleOutline className="text-indigo-200" />
@@ -29,8 +34,9 @@ export default (props) => {
                 </div>
 
             </div>
-            <button className="bg-indigo-600 text-white w-2/3 rounded p-1
+            <button className="bg-indigo-600 text-white w-2/3 rounded py-4
             hover:border-2">Escolher Plano</button>
+
         </div>
     )
 }
